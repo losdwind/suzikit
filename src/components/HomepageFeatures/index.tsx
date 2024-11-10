5d043c03634e6f9df77a7d9f21bd6d84a10ae93f
losdwind <s524256521@gmail.com>
2024-11-10 19:07:27 +0800

    setup: i18n

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,68 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import Translate, { translate } from "@docusaurus/Translate";
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  title: JSX.Element;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: <Translate id="homepage.features.easyToUse">Easy to Use</Translate>,
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <Translate id="homepage.features.easyToUse.description">
+          Docusaurus was designed from the ground up to be easily installed and
+          used to get your website up and running quickly.
+        </Translate>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: (
+      <Translate id="homepage.features.focusOnWhatMatters">
+        Focus on What Matters
+      </Translate>
+    ),
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <Translate
+          id="homepage.features.focusOnWhatMatters.description"
+          values={{ code: <code>docs</code> }}
+        >
+          {
+            "Docusaurus lets you focus on your docs, and we'll do the chores. Go ahead and move your docs into the {code} directory."
+          }
+        </Translate>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: (
+      <Translate id="homepage.features.poweredByReact">
+        Powered by React
+      </Translate>
+    ),
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <Translate id="homepage.features.extendOrCustomize">
+          Extend or customize your website layout by reusing React. Docusaurus
+          can be extended while reusing the same header and footer.
+        </Translate>
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
